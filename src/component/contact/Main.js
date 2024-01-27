50fe5ef4e6ccb6ec545fe37bb948c615960a37bb
Eshwar <reshwar2004@gmail.com>
2024-01-27 23:46:22 +0530

    added button to team page from contact us page

--- src/component/contact/Main.js
+++ src/component/contact/Main.js
@@ -4,12 +4,24 @@ import Brand from "../menhomethree/Brand";
 import Touch from "../mencontact/Touch";
 import Map from "../mencontact/Map";
 import Form from "../mencontact/Form";
+import { Link } from 'react-router-dom'
 
 function Main() {
   return (
     <>
         <Bredcom title="Home" subtitle="Contact Us"/>
         <Touch/>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+          <Link
+            to="/team"
+            className="btn ss-btn mr-15"
+            style={{ marginBottom: "20px" }}
+            data-animation="fadeInLeft"
+            data-delay=".4s"
+          >
+            Our Team
+          </Link>
+        </div>
         <Map/>
 
     </>
