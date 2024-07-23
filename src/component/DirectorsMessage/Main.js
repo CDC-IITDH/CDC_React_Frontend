caa2c5710f754df522c47cbaf6f5cde95f33ed64
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-07-24 00:06:41 +0530

    added Dean's message

--- src/component/DirectorsMessage/Main.js
+++ src/component/DirectorsMessage/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Bredcom from "../Bredcom/Main";
 import CssData from "../../CustomCSS/TestimonialthreeCSS.json";
 import directorImg from "./../../assets/img/director/Director.jpeg";
+import deanApImg from "./../../assets/img/director/dean_ap.jpg";
 import FICImg from "./../../assets/img/director/Satya_Priya_Image.jpeg";
 import FICImg2 from "./../../assets/img/team/ankit_v_k.JPG";
 
@@ -11,13 +12,22 @@ const Testimonialnew = () => {
       id: 1,
       title: " Director's Message",
       quote:
-        "Dear Esteemed HR Representatives, a warm welcome awaits you at the Indian Institute of Technology Dharwad (IITDh). It brings me immense joy to extend this invitation to your esteemed organizations for the placement of our talented students. Established in July 2016, IITDh has expanded significantly, offering a spectrum of programs from B.Tech to PhD across various disciplines. Presently situated in Permanent Campus of IIT Dharwad sprawling over 470 acres. Our library, boasting 5000 books and access to 8500 research journals, complements the academic vigor of our 1000 students, hailing from 21 states with a commendable 20% representation of women. With a faculty of 90 members, each holding prestigious qualifications and significant research accolades, our institute has garnered impressive sponsored projects and publications. It would be an honor to have your presence on our campus, providing an opportunity for our exceptional students to showcase their aptitude and readiness to contribute to your esteemed organization. We assure you of the quality and readiness of our students and eagerly anticipate fostering a lasting and mutually beneficial relationship with your organization through our placement drive.",
+        "Dear Esteemed HR Representatives, a warm welcome awaits you at the Indian Institute of Technology Dharwad (IITDh). It brings me immense joy to extend this invitation to your esteemed organizations for the placement of our talented students. Established in July 2016, IITDh has expanded significantly, offering a spectrum of programs from B.Tech to PhD across various disciplines. Presently situated in Permanent Campus of IIT Dharwad sprawling over 470 acres. Our library, boasting 5000 books and access to 8500 research journals, complements the academic vigor of our 1000 students, hailing from 21 states with a commendable 20% representation of women. With a faculty of 90 members, each holding prestigious qualifications and significant research accolades, our institute has garnered impressive sponsored projects and publications. It would be an honor to have your presence on our campus, providing an opportunity for our exceptional students to showcase their aptitude and readiness to contribute to your esteemed organization. We assure you of the quality and readiness of our students and eagerly anticipate fostering a lasting and mutually beneficial relationship with your organization through our placement drive. We look forward to hosting you.",
       authorName: "Venkappayya R Desai",
       authorRole: "Director, IIT Dharwad",
       imgSrc: directorImg,
     },
     {
       id: 2,
+      title: " Dean's Message",
+      quote:
+        "Dear Esteemed Guests, it brings me immense pleasure to extend a heartfelt invitation on behalf of the entire IIT Dharwad community. We cordially invite you to participate in our placement drive for final year students and internships for pre-final year students. At IIT Dharwad, our curriculum is meticulously crafted to offer a hands-on learning experience, focusing on imparting practical engineering skills essential for real-world contributions. Our courses integrate 'Course Projects' wherein students conceive and execute projects under the guidance of our esteemed faculty. With a spirit of collaboration, our vibrant and dedicated students actively contribute to their peers' learning through group projects, serving as teaching assistants, and even authoring manuals for courses or enhancing existing ones.As a growing institution, our students play an active role in shaping their academic environment, forming committees to enhance both their academic productivity and overall college experience. Each student brings their unique strengths to the table, collectively addressing and uplifting one another's weaknesses, thereby fostering an environment of continual growth.We eagerly anticipate your involvement in our placement sessions for final year students and internships for pre-final year students. We believe your visit will mark the beginning of a promising and enduring relationship between our esteemed Institute and your organization. Here's to the days ahead filled with collaboration and mutual growth.",
+      authorName: "Prof. Narayan S Punekar",
+      authorRole: "Dean of Academic Programmes, IIT Dharwad",
+      imgSrc: deanApImg,
+    },
+    {
+      id: 3,
       title: "Faculty In Charge's Message",
       quote:
         "It gives me great pleasure to invite you to visit Indian Institute of Technology, dhArwAD for the placement and internship season 2024-2025. We are confident that our students will deliver the things in real time environments and surpass all expectations when it comes to fulfilling organizational objectives. I look forward to continuing a fruitful relationship.",
@@ -26,7 +36,7 @@ const Testimonialnew = () => {
       imgSrc: FICImg,
     },
     {
-      id: 3,
+      id: 4,
       title: "Faculty In Charge's Message",
       quote:
         "Dear Esteemed Guests, it brings me immense pleasure to extend a heartfelt invitation on behalf of the entire IIT Dharwad community. We eagerly anticipate your involvement in hiring interns from pre-final year students. We believe your visit will mark the beginning of a promising and enduring relationship between our esteemed Institute and your organization.",
